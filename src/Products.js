139a03b98dea6e622830ad76ac5a3d85eb71eb4f
Dylan <89940659+dkoob@users.noreply.github.com>
2024-01-26 14:58:08 -0500

    Revert "Single Product and reviews"

--- src/Products.js
+++ src/Products.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import { NavLink, Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Products = ({
   products,
@@ -55,19 +55,13 @@ const Products = ({
         <div className="product" key={product.id}>
           <NavLink style={{padding: "0"}} to={`/products/${product.id}`}>
           <img src={product.product_image_path}></img>
-          <h5>
-            <Link to ={`/products/${product.id}`}>
-              {product.name}
-              </Link>
-              </h5>
+          <h5>{product.name}</h5>
           </NavLink>
         </div>
       ))}
     </div>
   );
   };
-
-  console.log(products.id)
 
   return (
     <main className="productContainerMain">
@@ -82,25 +76,7 @@ const Products = ({
         {displayProducts("tools")}
         <h2>Planters</h2>
         {displayProducts("planters")}
-
-
-
-
-
-
-
-
-
     </main>
-
-
-
-
-
-
-
-
-
   );
 };
 
